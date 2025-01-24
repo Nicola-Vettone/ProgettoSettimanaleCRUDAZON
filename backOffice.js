7f17768cdade3c421086ef5da3bd046e2b42f655
Nicola Vettone <nicolavettone57@gmail.com>
2025-01-24 16:13:57 +0100

    aggiustate parentesi

--- backOffice.js
+++ backOffice.js
@@ -1,10 +1,53 @@
-const URL = "https://striveschool-api.herokuapp.com/api/product/";
+const params = new URLSearchParams(window.location.search);
+const productId = params.get("appId");
 const form = document.querySelector("#form");
 
+const URL = productId ? "https://striveschool-api.herokuapp.com/api/product/" + productId : "https://striveschool-api.herokuapp.com/api/product/";
+
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNWIxMWI3NDcwMTAwMTU4YjJhZjYiLCJpYXQiOjE3Mzc3MTAzNTQsImV4cCI6MTczODkxOTk1NH0.1-0K95oHDGWa6RVnpJ4nP28N6kTtpBIWvhEbd2etRIo";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const inserisci = document.querySelector(".inserisci");
+  const modProduct = document.querySelector(".modifica");
+  //se è presente productId allora siamo in put
+  if (productId) {
+    modProduct.innerText = "Modifica prodotto";
+    modProduct.classList.add("btn-success");
+
+    fetch(URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Errore nel recupero del prodotto");
+        }
+      })
+      .then((product) => {
+        console.log("Dati prodotto:", product);
+
+        form.elements.name.value = product.name;
+        form.elements.brand.value = product.brand;
+        form.elements.description.value = product.description;
+        form.elements.imageUrl.value = product.imageUrl;
+        form.elements.price.value = product.price;
+      })
+      .catch((error) => console.error("Errore:", error));
+  } else {
+    inserisci.innerText = "Aggiungi prodotto";
+    inserisci.classList.add("btn-primary");
+  }
+});
+
 form.onsubmit = function (event) {
-  // evito il refresh del browser
   event.preventDefault();
-  // costruisco il dato a partire dai campi del form
+
   const newProduct = {
     name: form.elements.name.value,
     description: form.elements.description.value,
@@ -12,31 +55,31 @@ form.onsubmit = function (event) {
     imageUrl: form.elements.imageUrl.value,
     price: form.elements.price.value,
   };
-  // controllo la corretta creazione del dato
+
   console.log(newProduct);
 
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNWIxMWI3NDcwMTAwMTU4YjJhZjYiLCJpYXQiOjE3Mzc3MTAzNTQsImV4cCI6MTczODkxOTk1NH0.1-0K95oHDGWa6RVnpJ4nP28N6kTtpBIWvhEbd2etRIo";
-  fetch(URL, {
-    method: "POST",
+  //usiamo put o post
+  const methods = productId ? "PUT" : "POST";
+  const requestUrl = productId ? "https://striveschool-api.herokuapp.com/api/product/" + productId : "https://striveschool-api.herokuapp.com/api/product/";
+
+  fetch(requestUrl, {
+    method: methods,
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + token,
     },
-
     body: JSON.stringify(newProduct),
   })
     .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
-        console.error("Errore nel recupero dei prodotti:", response.status);
+        throw new Error("Errore nel salvataggio del prodotto");
       }
-      form.reset();
     })
     .then((data) => {
       console.log("Risposta del server:", data);
       form.reset();
     })
-    .catch((error) => console.error("Errore:", error)); // .catch correttamente qui
+    .catch((error) => console.error("Errore:", error));
 };
